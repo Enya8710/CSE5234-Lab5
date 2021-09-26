b26be15aaa7f2cbd067f9806cd9369edc0c8a879
Michael Frank <frank.522@osu.edu>
2021-09-26 17:06:50 -0400

    added images and description to product list page

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,43 +4,53 @@ export interface Product {
     price: number;
     description: string;
     quantity: number;
+    img_src: string;
   }
   
   export const products = [
     {
       id: 1,
-      name: 'Ivory Towers',
-      price: 4.79,
-      description: 'Succulent plants',
-      quantity: 0
+      name: 'Aoenium',
+      price: 7.99,
+      description:
+      'Aeoniums are most commonly known for their striking rosettes made up of dense, glossy, waxy leaves growing out of a single stem.',
+      quantity: 0,
+      img_src: "assets/imgs/aeonium.jpg"
+      
     },
     {
       id: 2,
-      name: 'Haworthia retusa f. geraldii',
-      price: 5.29,
-      description: 'Succulent plants',
-      quantity: 0
+      name: 'Kalanchoe',
+      price: 6.99,
+      description:
+        'Kalanchoe is a beautiful flowering plant that is both easy and rewarding to grow indoors or outdoors.',
+      quantity: 0,
+      img_src: "assets/imgs/kalanchoe.jpg"
     },
     {
       id: 3,
-      name: 'Crassula ovata',
-      price: 4.79,
-      description: 'Succulent plants',
-      quantity: 0
+      name: 'Senecio',
+      price: 2.99,
+      description: 'Senecio is a genus of flowering plants that contains more than 1000 species of groundsels, ragworts, and dusty miller.',
+      quantity: 0,
+      img_src: "assets/imgs/senecio.jpg"
     },
     {
       id: 4,
-      name: 'Haworthia Set of 5',
-      price: 17.49,
-      description: 'Succulent plants',
-      quantity: 0
+      name: 'Sempervivum',
+      price: 6.35,
+      description: 'Sempervivum is a genus of about 40 species of flowering plants that are commonly known as Houseleeks or Hen and Chicks.',
+      quantity: 0,
+      img_src: "assets/imgs/sempervivum.jpg"
     },
     {
       id: 5,
-      name: 'Crassula rogersii',
+      name: 'Crassula',
       price: 7.49,
-      description: 'Succulent plants',
-      quantity: 0
+      description: 'Crassula is a genus of succulent plants that includes more than 350 species native to many different parts of the world.',
+      quantity: 0,
+      img_src: "assets/imgs/crassula.jpg"
     },
   ];
+  
   
